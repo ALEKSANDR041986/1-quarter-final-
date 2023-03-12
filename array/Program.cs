ccf77a7cf1eb26099a57d4771d0c5afa4731e8b5
geekbrains <maksimov0904@icloud.com>
2023-03-12 21:22:36 +0500

    Создали массив из строковых элементов

--- array/Program.cs
+++ array/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите количество символов");
+int m = int.Parse(Console.ReadLine());
+PrintArray(array);
+
+// ------------------
+
+string[] Getarray(int m)
+{
+    string[] newArr = new string[m];
+    for (int i = 0; i < m; i++)
+    {
+        newArr[i] = Console.ReadLine();
+    }
+    return newArr;
+}
+
+// ----------------------
+
+void PrintArray(string[] newArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < newArray.Length; i++)
+    {
+        Console.Write($"{newArray[i]} ");
+    }
+    Console.Write("] -->>");
+}
