5c66012b1b50e3e09737d4283febda127c163de9
geekbrains <maksimov0904@icloud.com>
2023-03-12 21:26:22 +0500

    Создали массив из 1-3 элементов

--- array/Program.cs
+++ array/Program.cs
@@ -1,6 +1,8 @@
 ﻿Console.WriteLine("Введите количество символов");
 int m = int.Parse(Console.ReadLine());
+string[] array = Getarray(m);
 PrintArray(array);
+PrintRandomArray(array);
 
 // ------------------
 
@@ -24,4 +26,19 @@ void PrintArray(string[] newArray)
         Console.Write($"{newArray[i]} ");
     }
     Console.Write("] -->>");
+}
+
+// ------------------------
+
+void PrintRandomArray(string[] newArray)
+{
+    int l = new Random().Next(1, 4);
+    string[] randomArr = new string[l];
+    Console.Write("[");
+    for (int i = 0; i < l; i++)
+    {
+        randomArr[i] = newArray[new Random().Next(0, m - 1)];
+        Console.Write($"{randomArr[i]} ");
+    }
+    Console.Write("]");
 }
